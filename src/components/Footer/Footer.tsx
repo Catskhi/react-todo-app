0a2ab88a057185679df84dcafa530115908e9d02
Catskhi <catskhi@catskhi-pc>
2023-01-07 15:13:21 -0300

    Git correction done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,13 @@ export default function Footer () {
         <h4>By <a href='https://github.com/Catskhi/'
         rel='noreferrer'
         target={'_blank'}
-        className={styles.name}>Catskhi</a> @ 2023</h4>
+        className={styles.name + ' ' + styles.link}>Catskhi</a> @ <span className={styles.year}>2023</span>
+        |
+        <a href='https://github.com/Catskhi/react-todo-app'
+        rel='noreferrer'
+        target={'_blank'}
+        className={styles.link + ' ' + styles.projectPage}>Project Page</a>
+        </h4>
     </div>
   );
 }
